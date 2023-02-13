7e85fc128eb8d1465bf3b7ce47848772d2b0d18a
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-02-12 23:04:33 -0800

    Rewriting obsolete code.

---
+++ copy.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    // Get the text field
+    let copyText = document.getElementById("outputtext");
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
